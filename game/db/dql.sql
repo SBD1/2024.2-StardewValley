85160892cacf08f3d46047cfd68f2235e6d49a42
Gabriel Zaranza <gabrielzaranza@hotmail.com>
2025-01-13 18:42:37 -0300

    adicionando querys das habilidades no dql

--- game/db/dql.sql
+++ game/db/dql.sql
@@ -16,3 +16,41 @@ WHERE fk_jogador_id = %s
 SELECT * 
 FROM Ambiente 
 WHERE id_ambiente = %s
+
+--Listar as habilidades do jogador atravez do fk_habilidade_id(%s)
+SELECT * 
+FROM Habilidade 
+WHERE id_habilidade in (%s,%s,%s)
+
+--Retornar atributos da habilidade de mineração do jogador(%s)
+SELECT 
+    fk_Habilidade_id, 
+    nivel, 
+    reducaoEnergiaMinera,
+    minerioBonus,
+    xpMin,
+    xpMax 
+FROM habMineracao 
+WHERE fk_Habilidade_id = %s
+
+--Retornar atributos da habilidade de combate do jogador(%s)
+SELECT 
+    fk_Habilidade_id, 
+    nivel, 
+    vidaBonus,
+    danoBonus,
+    xpMin,
+    xpMax 
+FROM habCombate 
+WHERE fk_Habilidade_id = %s
+
+--Retornar atributos da habilidade de cultivo do jogador(%s)
+SELECT 
+    fk_Habilidade_id, 
+    nivel, 
+    cultivoBonus,
+    reducaoEnergiaCultiva,
+    xpMin,
+    xpMax 
+FROM habCultivo 
+WHERE fk_Habilidade_id = %s
