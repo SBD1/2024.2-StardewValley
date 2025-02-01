e6f32cbdd6a3269170ed0610edd8f19af337d614
Manuella Valadares <manuella.valadares@hotmail.com>
2025-02-01 19:09:53 -0300

    delete mapa

--- game/db/ddl.sql
+++ game/db/ddl.sql
@@ -4,11 +4,6 @@ CREATE TABLE IF NOT EXISTS Habilidade (
     tipo VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Mapa (
-    id_mapa SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Inimigo (
     id_inimigo SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -281,11 +276,11 @@ CREATE TABLE IF NOT EXISTS Instancia_Missao (
 CREATE TABLE IF NOT EXISTS Recompensa (
     id_Recompensa  SERIAL PRIMARY KEY,
     fk_Jogador_id INT NOT NULL,
-    id_item INT NOT NULL,
+    fk_id_item INT NOT NULL,
     fk_Instancia_Missao INT NOT NULL,
     quantidade INT NOT NULL,
     FOREIGN KEY (fk_Jogador_id) REFERENCES Jogador(id_jogador),
-    FOREIGN KEY (id_item) REFERENCES item(id_item),
+    FOREIGN KEY (fk_id_item) REFERENCES item(id_item),
     FOREIGN KEY (fk_Instancia_Missao) REFERENCES Instancia_Missao(id_Instancia_Missao)
 );
 
